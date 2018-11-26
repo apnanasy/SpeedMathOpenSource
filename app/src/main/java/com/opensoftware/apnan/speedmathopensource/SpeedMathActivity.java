79c8e9811eb6c26338f0b88cfd26eab205ee722f
ananasy <apnanasy1@gmail.com>
2018-11-26 08:20:31 -0500

    Initial Commit

---
+++ app/src/main/java/com/opensoftware/apnan/speedmathopensource/SpeedMathActivity.java
@@ -0,0 +1,13 @@
+package com.opensoftware.apnan.speedmathopensource;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpeedMathActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_speed_math);
+    }
+}
