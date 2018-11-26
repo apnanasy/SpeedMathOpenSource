16374632ad4420aafa924cb3c1c1b62addf2b4be
ananasy <apnanasy1@gmail.com>
2018-11-26 08:48:35 -0500

    Linked textViews and editTexts into main code from xml

--- app/src/main/java/com/opensoftware/apnan/speedmathopensource/SpeedMathActivity.java
+++ app/src/main/java/com/opensoftware/apnan/speedmathopensource/SpeedMathActivity.java
@@ -2,6 +2,9 @@ package com.opensoftware.apnan.speedmathopensource;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class SpeedMathActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class SpeedMathActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_speed_math);
+        TextView equationTV = findViewById(R.id.equationText);
+        TextView correctTV = findViewById(R.id.correctText);
+        TextView wrongTV = findViewById(R.id.wrongText);
+        EditText answerET = findViewById(R.id.answerEdit);
+
+        equationTV.setText("2x2");
     }
 }
