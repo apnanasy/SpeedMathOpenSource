f806283d1287be829be8ae2114393aa0fdc3ecdf
ananasy <apnanasy1@gmail.com>
2018-11-26 21:36:22 -0500

    Created RandMath class which will end up outputting all the equations and handling scoring.

---
+++ app/src/main/java/com/opensoftware/apnan/speedmathopensource/RandMath.java
@@ -0,0 +1,13 @@
+package com.opensoftware.apnan.speedmathopensource;
+
+public class RandMath {
+    private boolean mult;
+    public RandMath()
+    {
+        mult = true;
+    }
+    public String getEquation()
+    {
+        return "4 x 4";
+    }
+}
--- app/src/main/java/com/opensoftware/apnan/speedmathopensource/SpeedMathActivity.java
+++ app/src/main/java/com/opensoftware/apnan/speedmathopensource/SpeedMathActivity.java
@@ -1,22 +1,30 @@
 package com.opensoftware.apnan.speedmathopensource;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
 
 public class SpeedMathActivity extends AppCompatActivity {
+    private TextView equationTV, correctTV,wrongTV;
+    private EditText answerET;
+    private RandMath creator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_speed_math);
-        TextView equationTV = findViewById(R.id.equationText);
-        TextView correctTV = findViewById(R.id.correctText);
-        TextView wrongTV = findViewById(R.id.wrongText);
-        EditText answerET = findViewById(R.id.answerEdit);
+        equationTV = findViewById(R.id.equationText);
+        correctTV = findViewById(R.id.correctText);
+        wrongTV = findViewById(R.id.wrongText);
+        answerET = findViewById(R.id.answerEdit);
+        creator = new RandMath();
+        equationTV.setText(creator.getEquation());
 
-        equationTV.setText("2x2");
     }
+
+
 }
