c12c06958e41f10238f32050014083123008cad8
ananasy <apnanasy1@gmail.com>
2018-12-15 17:29:50 -0500

    Created the intro activity and the new custom game activity

--- app/src/main/java/com/opensoftware/apnan/speedmathopensource/Score.java
+++ app/src/main/java/com/opensoftware/apnan/speedmathopensource/Score.java
@@ -1,11 +1,17 @@
 package com.opensoftware.apnan.speedmathopensource;
 
+import java.util.ArrayList;
+
 public class Score {
     private Equation equate;
+    private ArrayList<Equation> equations;
+    private ArrayList<Integer> tried;
     private int tries;
     public Score()
     {
         tries = 0;
+        equations = new ArrayList<Equation>();
+        tried = new ArrayList<Integer>();
     }
     public void addEquation(Equation e)
     {
@@ -17,6 +23,8 @@ public class Score {
         tries++;
         if(answer == equate.getAnswer())
         {
+            equations.add(equate);
+            tried.add(tries);
             return true;
         } else {
             return false;
